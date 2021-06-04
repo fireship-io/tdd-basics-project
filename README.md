765adad4bcac8b75de387dc79409824f2480b1f5
Jeff Delaney <hello@fireship.io>
2021-06-04 08:51:00 -0700

    full working demo

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript TDD Project
+
+Example repo for [JavaScript Test-Driven Development with Jest](https://youtu.be/Jv2uxzhPFl4). 
+
+Challenge: Implement additional tests in the `test/stack.test.js` file. 
---
+++ test/stack.test.js
@@ -0,0 +1,40 @@
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+}
+
+describe('My Stack', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Stack();
+  });
+
+  it('is created empty', () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+
+  it('can push to the top', () => {
+    stack.push('🥑');
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe('🥑');
+
+    stack.push('🌽');
+    expect(stack.top).toBe(1);
+    expect(stack.peek).toBe('🌽');
+  });
+
+  it.todo('can pop off');
+});
